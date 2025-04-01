5f98be6b5e8f9a37e56c816580d4f7b55249a633
RamboLevi <platformerlevi@gmail.com>
2025-04-01 02:53:51 -0400

    yes

--- final_project/index.js
+++ final_project/index.js
@@ -11,7 +11,16 @@ app.use(express.json());
 app.use("/customer",session({secret:"fingerprint_customer",resave: true, saveUninitialized: true}))
 
 app.use("/customer/auth/*", function auth(req,res,next){
-//Write the authenication mechanism here
+    if (!req.session || !req.session.token) {
+        return res.status(401).json({ message: "Unauthorized: No token provided" });
+    }
+
+    jwt.verify(req.session.token, "secret_key", (err, decoded) => {
+        if (err) {
+            return res.status(403).json({ message: "Forbidden: Invalid token" });
+        }
+        req.user = decoded; // A dekódolt információt elmentjük a kérésbe
+        next();
 });
  
 const PORT =5000;
